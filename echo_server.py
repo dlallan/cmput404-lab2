1220d1796c630c194eac94977d4e6cbf3154ffb1
Dillon Allan <dallan@ualberta.ca>
2022-01-19 18:40:28 -0700

    Add listen forever and graceful shutdown to echo_server.py

--- echo_server.py
+++ echo_server.py
@@ -3,19 +3,29 @@ import socket
 
 def main():
     HOST = ''
-    PORT = 8888
+    PORT = 8001
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         server_socket.bind((HOST, PORT))
         print(f'Listening on port {PORT}')
+
+        # Listen until manually interrupted by user
         server_socket.listen()
-        client_conn, client_addr = server_socket.accept()
-        with client_conn:
-            print(f"client address: {client_addr}")
+        try:
             while True:
-                client_data = client_conn.recv(4096)
-                if not client_data:
-                    break
-                client_conn.sendall(client_data)
+                client_conn, client_addr = server_socket.accept()
+                with client_conn:
+                    print(f'Connected to client at address: {client_addr}')
+                    while True:
+                        client_data = client_conn.recv(1)
+                        if not client_data:
+                            client_conn.shutdown(socket.SHUT_RDWR)
+                            break
+                        client_conn.sendall(client_data)
+                        
+        except KeyboardInterrupt:
+            print('Shutting down')
+            server_socket.shutdown(socket.SHUT_RDWR)
 
 
 if __name__ == '__main__':
